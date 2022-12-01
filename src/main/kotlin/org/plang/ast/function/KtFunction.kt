50d6aa88c9e6d5df82c081e461fe0c418ff1c482
Patrick Arbeiter <patrick.arbeiter@leftshift.one>
2022-12-01 23:40:02 +0100

    cleanup KtFunction

--- src/main/kotlin/org/plang/ast/function/KtFunction.kt
+++ src/main/kotlin/org/plang/ast/function/KtFunction.kt
@@ -3,15 +3,21 @@ package org.plang.ast.function
 import org.plang.PLangContext
 import org.plang.api.IFunction
 
+
+typealias fun0 = () -> Any
+typealias fun1 = (arg1: Any) -> Any
+typealias fun2 = (arg1: Any, arg2: Any) -> Any
+
+@Suppress("UNCHECKED_CAST")
 class KtFunction(
     private val function: Any
 ) : IFunction {
 
     override fun run(args: List<Any>, ctx: PLangContext): Any {
         return when (args.size) {
-            0 -> (function as () -> Any)()
-            1 -> (function as (arg1: Any) -> Any)(args[0])
-            2 -> (function as (arg1: Any, arg2: Any) -> Any)(args[0], args[1])
+            0 -> (function as fun0)()
+            1 -> (function as fun1)(args[0])
+            2 -> (function as fun2)(args[0], args[1])
             else -> throw RuntimeException("argument size not matching")
         }
     }
